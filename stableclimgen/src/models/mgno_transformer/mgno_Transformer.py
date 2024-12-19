fixed masking issues and reversed cross attention

stableclimgen/src/models/mgno_transformer/mgno_Transformer.py
@@ -74,8 +74,8 @@ class MGNO_Transformer(nn.Module):
         self.model_dim_out = model_dim_out
         self.n_vars_total = n_vars_total
 
-        global_levels = [torch.tensor(block_conf.global_levels) for block_conf in block_configs] + [torch.tensor(0).view(-1)]
-        global_levels = torch.concat(global_levels).unique()
+        global_levels_tot = [torch.tensor(block_conf.global_levels) for block_conf in block_configs] 
+        global_levels = torch.concat(global_levels_tot+ [torch.tensor(0).view(-1)]).unique()
         
         self.register_buffer('global_levels', global_levels, persistent=False)
 
@@ -87,6 +87,7 @@ class MGNO_Transformer(nn.Module):
         for global_level in global_levels:
             grid_layers[str(int(global_level))] = GridLayer(global_level, mgrids[global_level]['adjc_lvl'], mgrids[global_level]['adjc_mask'], mgrids[global_level]['coords'], coord_system='polar')
 
+        n_no_layers_total = len((torch.concat(global_levels_tot)))
         n = 0
         global_level_in = 0
         # Construct blocks based on configurations
@@ -113,7 +114,7 @@ class MGNO_Transformer(nn.Module):
                                     params_learnable=block_conf.global_params_learnable[k],
                                     nh_projection=block_conf.nh_transformation[k],
                                     nh_backprojection=block_conf.nh_inverse_transformation[k],
-                                    precompute_coordinates=True if n!=0 and n<n_no_layers else False,
+                                    precompute_coordinates=True if n!=0 and n<n_no_layers_total else False,
                                     rotate_coordinate_system=rotate_coord_system)
 
                 no_layers.append(no_layer)
@@ -183,7 +184,7 @@ class MGNO_Transformer(nn.Module):
         :return: Output tensor of shape (batch_size, num_cells, output_dim).
         """
 
-        b,n,nh = x.shape[:3]
+        b,n,nh,nv,nc = x.shape[:5]
         x = x.view(b,n,nh,-1,self.model_dim_in)
 
         if mask is not None:
@@ -217,6 +218,8 @@ class MGNO_Transformer(nn.Module):
             
             # Process input through the block
             x, mask = block(x, coords_in=coords_in, coords_out=coords_out, indices_sample=indices_sample, mask=mask, emb=emb)
+            x = x.view(b,n,-1,nv,nc)
+            mask = mask.view(x.shape[:4])
 
         
         x = x.view(b,n,-1)

stableclimgen/src/models/mgno_vae/mgno_vae.py
@@ -252,7 +252,7 @@ class MGNO_VAE(nn.Module):
             coords_input = coords_input if k==0 else None
             x, mask = block(x, coords_in=coords_input, indices_sample=indices_sample, mask=mask, emb=emb)
 
-        x = self.vae_block.encode(x, coords_in=coords_input, indices_sample=indices_sample, mask=mask, emb=emb)
+        x, _ = self.vae_block.encode(x, coords_in=coords_input, indices_sample=indices_sample, mask=mask, emb=emb)
 
         x, mask = x.unsqueeze(dim=1), mask.unsqueeze(dim=1)
         x = self.quantization.quantize(x, indices_sample=indices_sample, emb=emb)
@@ -273,7 +273,7 @@ class MGNO_VAE(nn.Module):
         x = self.quantization.post_quantize(x, indices_sample=indices_sample, emb=emb)
         x = x.squeeze(dim=1)
 
-        x = self.vae_block.decode(x, indices_sample=indices_sample, emb=emb)
+        x, _ = self.vae_block.decode(x, indices_sample=indices_sample, emb=emb)
 
         for k, block in enumerate(self.decoder_only_blocks):
             

stableclimgen/src/modules/neural_operator/neural_operator.py
@@ -301,18 +301,12 @@ class Normal_NoLayer(NoLayer):
         weights = weights.view(*c_shape, len(self.mus_lon), len(self.mus_lat))
 
         if mask is not None:
-       #     mask = mask.view(*c_shape)
-       #     mask = torch.logical_or(mask, mask_cut)
             mask = mask.view(*c_shape,1,1)
-        else:
-            mask = mask_cut.repeat_interleave(nv, dim=-1).view(*c_shape,1,1)
-        
-        weights_weights = (mask == False)
-        weights = (weights * weights_weights)
-
-        mask = mask.view(*c_shape)
-        mask = mask.sum(dim=-2)==mask.shape[-2]
-        mask = mask.unsqueeze(dim=-2).repeat_interleave(seq_ref, dim=-2)
+            weights_weights = (mask == False)
+            weights = (weights * weights_weights)
+            mask = mask.view(*c_shape)
+            mask = mask.sum(dim=-2)==mask.shape[-2]
+            mask = mask.unsqueeze(dim=-2).repeat_interleave(seq_ref, dim=-2)
         
         weights = weights/(weights.sum(dim=3,keepdim=True)+1e-10)
 
@@ -340,6 +334,8 @@ class Normal_NoLayer(NoLayer):
         x = x.view(-1, n, seq_ref, 1, nv, n_lon, n_lat, nc)
 
         if mask is not None:
+            mask_norm = mask.view(b,n, seq_ref, 1, nv, 1, 1, 1)
+            weights = weights.masked_fill(mask_norm, 0)
             mask = mask.view(b,n,seq_ref,nv)
             mask = mask.sum(dim=-2)==mask.shape[-2]
             mask = mask.unsqueeze(dim=-2).repeat_interleave(seq_in, dim=-2)
@@ -430,17 +426,17 @@ class polNormal_NoLayer(NoLayer):
        #     mask = mask.view(*c_shape)
         #    mask = torch.logical_or(mask, mask_cut.view(*c_shape[:-1],1))
             mask = mask.view(*c_shape,1,1)
-        else:
-            mask = mask_cut.repeat_interleave(nv, dim=-1).view(*c_shape[:-1],1)
         
-        weights_weights = (mask == False)
-        weights = (weights * weights_weights)
+            weights_weights = (mask == False)
+            weights = (weights * weights_weights)
 
-        mask = mask.view(*c_shape)
-        mask = mask.sum(dim=-2)==mask.shape[-2]
-        mask = mask.unsqueeze(dim=-2).repeat_interleave(seq_ref, dim=-2)
+            norm = weights_weights.sum(dim=3, keepdim=True) 
+            mask = mask.view(*c_shape)
+            mask = mask.sum(dim=-2)==mask.shape[-2]
+            mask = mask.unsqueeze(dim=-2).repeat_interleave(seq_ref, dim=-2)
 
         weights = weights/(weights.sum(dim=3,keepdim=True)+1e-10)
+        #weights = weights/(norm+1e-10)
 
         x = x.view(*c_shape,1, 1, nc)
         x = (x * weights.unsqueeze(dim=-1)).sum(dim=3)
@@ -466,6 +462,10 @@ class polNormal_NoLayer(NoLayer):
         x = x.view(b, n, seq_ref, 1, nv, n_lon, n_lat, nc)
 
         if mask is not None:
+            #mask = mask.view(b,n, seq_ref, 1, nv, 1, 1, 1)
+            mask_norm = mask.view(b,n, seq_ref, 1, nv, 1, 1, 1)
+            weights = weights.masked_fill(mask_norm, 0)
+
             mask = mask.view(b,n,seq_ref,nv)
             mask = mask.sum(dim=-2)==mask.shape[-2]
             mask = mask.unsqueeze(dim=-2).repeat_interleave(seq_in, dim=-2)

stableclimgen/src/modules/neural_operator/no_blocks.py
@@ -28,6 +28,7 @@ class NOBlock(nn.Module):
                  embed_confs: Dict = None,
                  embed_mode: str = "sum",
                  spatial_attention_config={},
+                 NO_weights=None
                 ) -> None: 
       
         super().__init__()
@@ -114,25 +115,33 @@ class NOBlock(nn.Module):
         x, mask = self.no_layer.transform(x, coordinates=coords_in, indices_sample=indices_sample, mask=mask)
 
         for layer in self.att_block_types_encode:
-            if isinstance(layer, NHAttention) or isinstance(layer, SpatialAttention):
+            if isinstance(layer, SpatialAttention):
                 x = layer(x, indices_sample=indices_sample, mask=mask, emb=emb)
+            elif isinstance(layer, NHAttention):
+                x, mask = layer(x, indices_sample=indices_sample, mask=mask, emb=emb)
             else:
                 emb = self.check_add_coordinate_embeddings(emb, indices_sample) if self.prepare_coordinates else emb
-                x = layer(x, mask=mask, emb=emb)
+                x, mask = layer(x, mask=mask, emb=emb)
 
         return x, mask
 
 
-    def decode(self, x, coords_out=None, indices_sample=None, mask=None, emb=None):
+    def decode(self, x, coords_out=None, indices_sample=None, mask=None, emb=None, inv_transform_mask=None):
         
         for layer in self.att_block_types_decode:
-            if isinstance(layer, NHAttention) or isinstance(layer, SpatialAttention):
+            if isinstance(layer, SpatialAttention):
                 x = layer(x, indices_sample=indices_sample, mask=mask, emb=emb)
+            elif isinstance(layer, NHAttention):
+                x, mask = layer(x, indices_sample=indices_sample, mask=mask, emb=emb)
             else:
                 emb = self.check_add_coordinate_embeddings(emb, indices_sample) if self.prepare_coordinates else emb
                 x = layer(x, mask=mask, emb=emb)
 
-        x, mask = self.no_layer.inverse_transform(x, coordinates=coords_out, indices_sample=indices_sample, mask=mask)
+        if mask is not None:
+            if inv_transform_mask is None:
+                inv_transform_mask = mask 
+
+        x, mask = self.no_layer.inverse_transform(x, coordinates=coords_out, indices_sample=indices_sample, mask=inv_transform_mask)
 
         return x, mask
     
@@ -164,7 +173,8 @@ class Serial_NOBlock(nn.Module):
                  embed_names_decode: List[List[str]] = None,
                  embed_confs: Dict = None,
                  embed_mode: str = "sum",
-                 spatial_attention_configs: dict = {}
+                 spatial_attention_configs: dict = {},
+                 Block_NO_weights=None
                 ) -> None: 
       
         super().__init__()
@@ -228,7 +238,8 @@ class Stacked_NOBlock(nn.Module):
                  embed_names_decode: List[List[str]] = None,
                  embed_confs: Dict = None,
                  embed_mode: str = "sum",
-                 spatial_attention_configs = {}
+                 spatial_attention_configs = {},
+                 Block_NO_weights=None
                 ) -> None: 
       
         super().__init__()
@@ -273,7 +284,7 @@ class Stacked_NOBlock(nn.Module):
             x, mask = layer.encode(x, coords_in=coords_in, indices_sample=indices_sample, mask=mask, emb=emb)
             x, mask = shape_to_att(x, mask=mask)
 
-        return x
+        return x, mask
     
     def decode(self, x, coords_out=None, indices_sample=None, mask=None, emb=None):
 
@@ -283,15 +294,15 @@ class Stacked_NOBlock(nn.Module):
             x = shape_to_x(x, x_shape_origin)
             x, mask = layer.decode(x, coords_out=coords_out, indices_sample=indices_sample, emb=emb)
 
-        return x
+        return x, mask
 
 
     
     def forward(self, x, coords_in=None, coords_out=None, indices_sample=None, mask=None, emb=None):
         
-        x = self.encode(x, coords_in=coords_in, indices_sample=indices_sample, mask=mask, emb=emb)
+        x, mask = self.encode(x, coords_in=coords_in, indices_sample=indices_sample, mask=mask, emb=emb)
 
-        x = self.decode(x, coords_out=coords_out, indices_sample=indices_sample, mask=mask, emb=emb)
+        x, mask = self.decode(x, coords_out=coords_out, indices_sample=indices_sample, mask=mask, emb=emb)
 
         return x, mask
 
@@ -313,7 +324,8 @@ class UNet_NOBlock(nn.Module):
                  embed_names_decode: List[List[str]] = None,
                  embed_confs: Dict = None,
                  embed_mode: str = "sum",
-                 spatial_attention_configs = {}
+                 spatial_attention_configs = {},
+                 Block_NO_weights=None
                 ) -> None: 
       
         super().__init__()
@@ -366,21 +378,24 @@ class UNet_NOBlock(nn.Module):
 
         return x_skip, mask_skip
     
-    def decode(self, x_skip, mask_skip, coords_out=None, indices_sample=None, emb=None):
-
+    def decode(self, x_skip, masks_skip, coords_out=None, indices_sample=None, emb=None):
+        
+        mask = masks_skip[-1] 
         for layer_idx in range(len(self.NO_Blocks)-1,-1,-1):
             
-            mask = mask_skip[layer_idx] 
             layer = self.NO_Blocks[layer_idx]
             x_shape_origin = (*x_skip[layer_idx].shape[:3], *layer.n_params, -1)
 
             if layer_idx<len(self.NO_Blocks)-1:
-                x = torch.cat((shape_to_x(x, x_shape_origin),
-                               shape_to_x(x_skip[layer_idx], x_shape_origin)), dim=-1)
+            #   x = torch.cat((shape_to_x(x, x_shape_origin),
+             #                  shape_to_x(x_skip[layer_idx], x_shape_origin)), dim=-1)
+                x = torch.cat((shape_to_x(x_skip[layer_idx], x_shape_origin),
+                               shape_to_x(x, x_shape_origin)), dim=-1)
             else:
                 x = shape_to_x(x_skip[layer_idx], x_shape_origin)
-                              
-            x, mask = layer.decode(x, coords_out=coords_out, indices_sample=indices_sample, mask=mask, emb=emb)
+            
+           # coords_out_ = None if layer_idx !=0 else coords_out
+            x, mask = layer.decode(x, coords_out=coords_out, indices_sample=indices_sample, mask=masks_skip[layer_idx], emb=emb, inv_transform_mask=mask)
 
         return x
 
@@ -412,7 +427,8 @@ class Parallel_NOBlock(nn.Module):
                  embed_names_decode: List[List[str]] = None,
                  embed_confs: Dict = None,
                  embed_mode: str = "sum",
-                 spatial_attention_configs = {}
+                 spatial_attention_configs = {},
+                 Block_NO_weights=None
                 ) -> None: 
       
         super().__init__()
@@ -538,7 +554,7 @@ class ParamAttention(nn.Module):
 
         x, _ = shape_to_att(x, self.param_idx_att)
         
-        x, _  = self.attention_layer(x, mask=mask)
+        x, mask  = self.attention_layer(x, mask=mask)
 
         x = x_res + self.gamma*x
 
@@ -553,7 +569,13 @@ class ParamAttention(nn.Module):
         x = x_res + self.gamma_mlp*x
 
         x = shape_to_x(x, x_shape, self.param_idx_att)
-        return x
+
+        if mask is not None:
+            mask = mask.view(*x.shape[:3],-1)
+            mask = mask.sum(dim=-1).bool()
+            
+
+        return x, mask
     
 class CrossAttention(nn.Module):
   
@@ -644,12 +666,13 @@ class NHAttention(nn.Module):
 
         x, mask = self.no_layer_nh.transform(x, indices_sample=indices_sample, mask=mask)
 
-        x = self.attention_layer(x, mask=mask, emb=emb)
+        x, mask = self.attention_layer(x, mask=mask, emb=emb)
  
         x, mask = self.no_layer_nh.inverse_transform(x, indices_sample=indices_sample, mask=mask)
 
         x = shape_to_x(x, x_shape)
-        return x
+
+        return x, mask
 
 
 