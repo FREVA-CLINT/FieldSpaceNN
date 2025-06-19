4ebeb9ea16982d69be5b0be864e54c5aadd9397b
johannesmeuer <meuer@dkrz.de>
2025-06-19 12:16:10 +0200

    outsource instantiations

--- stableclimgen/src/decode.py
+++ stableclimgen/src/decode.py
@@ -25,22 +25,59 @@ except ValueError as e:
     raise
 MAX_INDEX = (END_DATE - START_DATE).days
 
+data_file="/work/bk1318/k204233/stableclimgen/evaluations/mgno_ngc/vae_16compress_vonmises_crosstucker_unetlike/tas_1940-2050_merged.zarr"
+config_path = '/work/bk1318/k204233/stableclimgen/snapshots/mgno_ngc/vae_16compress_vonmises_crosstucker_unetlike'
+with initialize_config_dir(config_dir=config_path, job_name="your_job"):
+    cfg = compose(config_name="composed_config", strict=False)
 
-def decode(timesteps, variables, region=-1, compression_factor=16,
-           data_file="/work/bk1318/k204233/stableclimgen/evaluations/mgno_ngc/vae_{}compress_vonmises_crosstucker_unetlike/tas_1940-2050_merged.zarr") -> None:
+norm_dict = {"tas": {"normalizer": {"class": "QuantileNormalizer", "quantile": 0.01},
+                     "stats": {"quantiles": {"0.01": 234.30984497,
+                                             "0.99": 305.74804688}}},
+             "pr": {"normalizer": {"class": "QuantileNormalizer", "quantile": 0.01},
+                    "stats": {"quantiles": {"0.01": 0.0, "0.99": 0.00055823}}},
+             "pres_sfc": {"normalizer": {"class": "QuantileNormalizer", "quantile": 0.01},
+                          "stats": {"quantiles": {"0.01": 64049.64453125,
+                                                  "0.99": 103337.53125}}},
+             "uas": {"normalizer": {"class": "QuantileNormalizer", "quantile": 0.01},
+                     "stats": {"quantiles": {"0.01": -12.04504204,
+                                             "0.99": 17.22920036}}},
+             "vas": {"normalizer": {"class": "QuantileNormalizer", "quantile": 0.01},
+                     "stats": {"quantiles": {"0.01": -12.20680428,
+                                             "0.99": 11.26358509}}}}
+
+cfg.dataloader.dataset.norm_dict = norm_dict
+cfg.dataloader.datamodule.num_workers = 1
+cfg.dataloader.datamodule.batch_size = 1
+cfg.dataloader.dataset.p_dropout = 0
+cfg.dataloader.dataset.random_p = False
+cfg.dataloader.dataset.in_nside = 256
+cfg.dataloader.dataset.search_radius = 4
+cfg.dataloader.dataset.nh_input = 3
+cfg.dataloader.dataset.coarsen_sample_level = 7
+cfg.dataloader.dataset.deterministic = True
+cfg.dataloader.dataset.n_sample_vars = 1
+cfg.dataloader.dataset.bottleneck_in_nside = 32
+cfg.ckpt_path = os.path.join(config_path, 'last.ckpt')
+cfg.trainer.accelerator = 'gpu'
+cfg.trainer.precision = 32
+cfg.model.mode = "decode"
+cfg.export_to_zarr = True
+
+# Initialize model and trainer
+model = instantiate(cfg.model)
+trainer = instantiate(cfg.trainer)
+
+
+def decode(timesteps, variables, region=-1) -> None:
     save_stdout = sys.stdout
     sys.stdout = open('trash', 'w')
 
     timesteps = [date_to_index(ts) for ts in timesteps]
 
-    config_path = '/work/bk1318/k204233/stableclimgen/snapshots/mgno_ngc/vae_16compress_vonmises_crosstucker_unetlike'
-    with initialize_config_dir(config_dir=config_path, job_name="your_job"):
-        cfg = compose(config_name="composed_config", strict=False)
-
     data_dict = {
         "test": {
             "source":
-                {"files": [data_file.format(compression_factor)],
+                {"files": data_file,
                  "variables": variables},
             "target": {"files": [None],
                        "variables": variables},
@@ -48,44 +85,9 @@ def decode(timesteps, variables, region=-1, compression_factor=16,
         }
     }
 
-    norm_dict = {"tas": {"normalizer": {"class": "QuantileNormalizer", "quantile": 0.01},
-                         "stats": {"quantiles": {"0.01": 234.30984497,
-                                                 "0.99": 305.74804688}}},
-                 "pr": {"normalizer": {"class": "QuantileNormalizer", "quantile": 0.01},
-                        "stats": {"quantiles": {"0.01": 0.0, "0.99": 0.00055823}}},
-                 "pres_sfc": {"normalizer": {"class": "QuantileNormalizer", "quantile": 0.01},
-                              "stats": {"quantiles": {"0.01": 64049.64453125,
-                                                      "0.99": 103337.53125}}},
-                 "uas": {"normalizer": {"class": "QuantileNormalizer", "quantile": 0.01},
-                         "stats": {"quantiles": {"0.01": -12.04504204,
-                                                 "0.99": 17.22920036}}},
-                 "vas": {"normalizer": {"class": "QuantileNormalizer", "quantile": 0.01},
-                         "stats": {"quantiles": {"0.01": -12.20680428,
-                                                 "0.99": 11.26358509}}}}
 
     cfg.dataloader.dataset.data_dict = data_dict
-    cfg.dataloader.dataset.norm_dict = norm_dict
-    cfg.dataloader.datamodule.num_workers = 1
-    cfg.dataloader.datamodule.batch_size = 1
-    cfg.dataloader.dataset.p_dropout = 0
-    cfg.dataloader.dataset.random_p = False
-    cfg.dataloader.dataset.in_nside = 256
-    cfg.dataloader.dataset.search_radius = 4
-    cfg.dataloader.dataset.nh_input = 3
-    cfg.dataloader.dataset.coarsen_sample_level = 7
-    cfg.dataloader.dataset.deterministic = True
-    cfg.dataloader.dataset.n_sample_vars = 1
-    cfg.dataloader.dataset.bottleneck_in_nside = 32
     cfg.dataloader.dataset.region = region
-    cfg.ckpt_path = os.path.join(config_path, 'last.ckpt')
-    cfg.trainer.accelerator = 'gpu'
-    cfg.trainer.precision = 32
-    cfg.model.mode = "decode"
-    cfg.export_to_zarr = True
-
-    # Initialize model and trainer
-    model = instantiate(cfg.model)
-    trainer = instantiate(cfg.trainer)
 
     test_dataset = instantiate(cfg.dataloader.dataset,
                                data_dict=data_dict["test"],
