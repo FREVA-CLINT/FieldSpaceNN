5a7357ae0db5b487b175c9f100981f28fc896ffa
Maximilian Witte <max-witte@gmx.de>
2025-06-30 11:16:21 +0200

    to last commit

--- stableclimgen/src/modules/multi_grid/processing.py
+++ stableclimgen/src/modules/multi_grid/processing.py
@@ -1,5 +1,7 @@
 from typing import List, Dict
 
+from omegaconf import ListConfig
+
 import torch
 import torch.nn as nn
 
@@ -9,10 +11,10 @@ from ..base import LinEmbLayer
 from ..grids.grid_layer import GridLayer
 
 from ...modules.embedding.embedder import get_embedder
-from .mg_attention import GridCrossAttention,GridSelfAttention
+from .mg_attention import GridSelfAttention
 from .mg_base import NHConv,ResNHConv
 
-class MG_Block(nn.Module):
+class MG_SingleBlock(nn.Module):
   
     def __init__(self,
                  grid_layers: Dict[str,GridLayer],
@@ -32,13 +34,13 @@ class MG_Block(nn.Module):
         self.blocks = nn.ModuleDict()
         self.grid_layers = grid_layers
 
-        min_zoom = layer_settings.get("min_zoom", max(in_zooms))
+        max_zoom = layer_settings.get("max_zoom", max(in_zooms))
 
         for in_features, out_features, zoom in zip(in_features_list, out_features_list, in_zooms):
             
             type = layer_settings.get('type','TransformerBlock')
 
-            if zoom > min_zoom:
+            if zoom > max_zoom:
                 block = LinEmbLayer(
                     in_features,
                     out_features, 
@@ -89,7 +91,17 @@ class MG_Block(nn.Module):
                         ranks_spatial=ranks_spatial,
                         layer_confs=layer_confs
                     )
-        
+
+                elif type == 'linear':
+                    ranks_spatial = layer_settings.get('ranks_spatial', [])
+
+                    block = LinEmbLayer(
+                        in_features,
+                        out_features, 
+                        layer_norm=False, 
+                        identity_if_equal=False,
+                        layer_confs=layer_confs)
+
             self.blocks[str(zoom)] = block
         
 
@@ -105,104 +117,73 @@ class MG_Block(nn.Module):
         return x_zooms
     
 
-
-class MG_CrossBlock(nn.Module):
+class MG_MultiBlock(nn.Module):
   
     def __init__(self, 
                  grid_layers: Dict[str, GridLayer],
                  in_zooms: List[int],
                  layer_settings: Dict,
-                 in_features_list: List[int],
-                 layer_confs={}
-                 ) -> None:
-        super().__init__()
-        
-        max_zoom_diff = layer_settings.get("max_zoom_diff", max(in_zooms)-min(in_zooms))
-        asc = layer_settings['type'] == 'ascending'
-
-        self.out_features = in_features_list
-        self.out_zooms = in_zooms
-        self.blocks = nn.ModuleDict()
-
-        for k, in_zoom in enumerate(in_zooms):
-            embedder_q = get_embedder(**layer_settings.get('embed_confs', {}), zoom=in_zoom)
-
-            grid_layer_cross = grid_layers[str(in_zoom)]
-            embedders_kv = {}
-            for in_zoom_cross in in_zooms:
-                
-                zoom_diff = in_zoom - in_zoom_cross
-                if (asc and zoom_diff > 0) or (zoom_diff < 0) and (abs(zoom_diff) > max_zoom_diff):
-                    grid_layer_cross = grid_layers[str(in_zoom_cross)] if grid_layers[str(in_zoom_cross)].zoom < grid_layer_cross.zoom else grid_layer_cross
-                    embedders_kv[str(in_zoom_cross)] = get_embedder(**layer_settings.get('embed_confs', {}), zoom=in_zoom_cross)
-
-            if len(embedders_kv)>0:
-                block = GridCrossAttention(grid_layers[str(in_zoom)],
-                                        grid_layer_cross,
-                                        in_features_list[k],
-                                        in_features_list[k],
-                                        mult = layer_settings.get("mlp_mult",1),
-                                        num_heads = layer_settings.get("num_heads",1),
-                                        n_head_channels = layer_settings.get("n_head_channels",None),
-                                        embedder_q=embedder_q,
-                                        embedders_kv=embedders_kv,
-                                        layer_confs=layer_confs,
-                                        )
-                self.blocks[str(in_zoom)] = block
-
-    def forward(self, x_zooms, emb=None, mask_zooms=None, sample_dict={}):
-
-        for block in self.blocks.values():
-            x_zooms = block(x_zooms, emb=emb, mask_zooms=mask_zooms, sample_dict=sample_dict)
-
-        return x_zooms
-    
-
-class MG_SelfBlock(nn.Module):
-  
-    def __init__(self, 
-                 grid_layers: Dict[str, GridLayer],
-                 in_zooms: List[int],
-                 layer_settings: Dict,
-                 in_features_list: List[int],
+                 in_features: int,
                  out_features: int,
+                 q_zooms:List|int = -1,
+                 kv_zooms:List|int = -1,
                  layer_confs={}
                  ) -> None:
         super().__init__()
         
-        self.out_features = [out_features]*len(in_features_list)
+        self.out_features = [out_features]*len(in_zooms)
         self.out_zooms = in_zooms
         self.blocks = nn.ModuleDict()
+
+        if isinstance(in_features, (List,ListConfig)) and len(set(in_features)) > 1:
+            raise ValueError("features of levels should be the same.")
+        elif isinstance(in_features, (int)):
+            in_features = in_features
+        else:
+            in_features = in_features[0]
+
+        if not isinstance(q_zooms, (List,ListConfig)) and (q_zooms == -1):
+            q_zooms = in_zooms
+        
+        if not isinstance(kv_zooms,(List,ListConfig)) and (kv_zooms == -1):
+            kv_zooms = in_zooms
 
         grid_layer = grid_layers[str(in_zooms[0])]
 
         embedders_kv = {}
         embedders_q = {}
 
-        for k, zoom in enumerate(in_zooms):
+        for k, zoom in enumerate(q_zooms):
+            if zoom not in in_zooms:
+                raise ValueError(f"Zoom level {zoom} at index {k} of q_zooms not found in in_zooms")
+            
             embedder_q = get_embedder(**layer_settings.get('embed_confs', {}), zoom=zoom)
-            embedder_kv = get_embedder(**layer_settings.get('embed_confs', {}), zoom=zoom)
-
-            grid_layer = grid_layers[str(zoom)] if grid_layers[str(zoom)].zoom < grid_layer.zoom else grid_layer
-
             embedders_q[str(zoom)] = embedder_q
+            
+        
+        for k, zoom in enumerate(kv_zooms):
+            if zoom not in in_zooms:
+                raise ValueError(f"Zoom level {zoom} at index {k} of kv_zooms not found in in_zooms")
+            
+            embedder_kv = get_embedder(**layer_settings.get('embed_confs', {}), zoom=zoom)
             embedders_kv[str(zoom)] = embedder_kv
-
-       #     in_features[str(zoom)] = in_features_list[k]
-
+     
+        grid_layer = grid_layers[str(min(q_zooms + kv_zooms))] 
             
         block = GridSelfAttention(grid_layer,
-                                in_features_list[0],
+                                in_features,
                                 out_features,
                                 mult = layer_settings.get("mlp_mult",1),
                                 num_heads = layer_settings.get("num_heads",1),
                                 n_head_channels = layer_settings.get("n_head_channels",None),
                                 with_nh= layer_settings.get("with_nh",True),
+                                var_att= layer_settings.get("var_att",False),
+                                seq_length= layer_settings.get("seq_length",0),
                                 common_kv = layer_settings.get("common_kv",False),
                                 common_q = layer_settings.get("common_q",False),
                                 embedders_q=embedders_q,
                                 embedders_kv=embedders_kv,
-                                layer_confs=layer_confs,
+                                layer_confs=layer_confs
                                 )
         self.block = block
 
