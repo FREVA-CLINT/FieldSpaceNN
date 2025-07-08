82fc078e24423375912f28efed8d1c1b1b54b3fc
Maximilian Witte <max-witte@gmx.de>
2025-07-08 16:43:14 +0200

    added out_zooms to self grid attention

--- stableclimgen/src/modules/multi_grid/processing.py
+++ stableclimgen/src/modules/multi_grid/processing.py
@@ -133,6 +133,7 @@ class MG_MultiBlock(nn.Module):
     def __init__(self, 
                  grid_layers: Dict[str, GridLayer],
                  in_zooms: List[int],
+                 out_zooms: List[int],
                  layer_settings: Dict,
                  in_features: int,
                  out_features: int,
@@ -144,8 +145,16 @@ class MG_MultiBlock(nn.Module):
                  ) -> None:
         super().__init__()
         
-        self.out_features = [out_features]*len(in_zooms)
-        self.out_zooms = in_zooms
+        self.out_features = [out_features]*len(out_zooms)
+        self.out_zooms = out_zooms
+
+        self.zero_zooms_gen = nn.ParameterDict()
+
+        for zoom in out_zooms:
+            if zoom not in in_zooms:
+                self.zero_zooms_gen[str(zoom)] = nn.Parameter(torch.zeros((1,1,1,12*4**zoom,1)), requires_grad=False)
+
+
         self.blocks = nn.ModuleDict()
 
         if isinstance(in_features, (List,ListConfig)) and len(set(in_features)) > 1:
@@ -167,8 +176,8 @@ class MG_MultiBlock(nn.Module):
         embedders_q = {}
 
         for k, zoom in enumerate(q_zooms):
-            if zoom not in in_zooms:
-                raise ValueError(f"Zoom level {zoom} at index {k} of q_zooms not found in in_zooms")
+            #if zoom not in in_zooms:
+            #    raise ValueError(f"Zoom level {zoom} at index {k} of q_zooms not found in in_zooms")
             
             embedder_q = get_embedder(**layer_settings.get('embed_confs', {}), grid_layer=grid_layers[str(mg_emb_zoom)], zoom=zoom)
             embedders_q[str(zoom)] = embedder_q
@@ -200,9 +209,28 @@ class MG_MultiBlock(nn.Module):
                                 )
         self.block = block
 
-    def forward(self, x_zooms, emb=None, mask_zooms=None, sample_dict={}):
+    def generate_zoom(self, x: torch.Tensor, zoom, sample_dict={}):
+        b,nv,t,s,f = x.shape
 
+        x_zoom = self.zero_zooms_gen[str(zoom)]
+
+        if 'zoom_patch_sample' in sample_dict.keys():
+            x_zoom = x_zoom.view(1,1,1,12*4**sample_dict['zoom_patch_sample'],-1,1)[:,:,:,0]
+
+        return x_zoom.expand(b,nv,t,-1,f)
+
+
+    def forward(self, x_zooms, emb=None, mask_zooms={}, sample_dict={}):
+
+        for zoom in self.out_zooms:
+            if zoom not in x_zooms.keys():
+                x = self.generate_zoom(list(x_zooms.values())[0], zoom, sample_dict=sample_dict)
+                x_zooms[zoom] = x
     
         x_zooms = self.block(x_zooms, emb=emb, mask_zooms=mask_zooms, sample_dict=sample_dict)
 
-        return x_zooms
+        x_zooms_out = {}
+        for zoom in self.out_zooms:
+            x_zooms_out[zoom] = x_zooms[zoom]
+
+        return x_zooms_out
