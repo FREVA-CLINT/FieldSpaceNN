13e276cb6881b3855220f1c6d555a3c920d73268
johannesmeuer <meuer@dkrz.de>
2025-07-24 08:04:38 +0200

    fix probabilistic inference

--- stableclimgen/src/models/mg_transformer/pl_mg_probabilistic.py
+++ stableclimgen/src/models/mg_transformer/pl_mg_probabilistic.py
@@ -10,16 +10,17 @@ class LightningProbabilisticModel(pl.LightningModule):
 
     def predict_step(self, batch, batch_index):
         source, target, coords_input, coords_output, sample_dict, mask, emb, rel_dists_input, _ = batch
-        batch_size = source.shape[0]
+        max_zoom = max(target.keys())
+        batch_size = target[max_zoom].shape[0]
         total_samples = batch_size * self.n_samples  # Total expanded batch size
 
         # Repeat each sample n_samples times
-        source = source.repeat_interleave(self.n_samples, dim=0)
-        target = target.repeat_interleave(self.n_samples, dim=0)
+        source = {int(zoom): source[zoom].repeat_interleave(self.n_samples, dim=0) for zoom in source.keys()}
+        target = {int(zoom): target[zoom].repeat_interleave(self.n_samples, dim=0) for zoom in target.keys()}
         coords_input = coords_input.repeat_interleave(self.n_samples, dim=0)
         coords_output = coords_output.repeat_interleave(self.n_samples, dim=0)
         rel_dists_input = rel_dists_input.repeat_interleave(self.n_samples, dim=0)
-        mask = mask.repeat_interleave(self.n_samples, dim=0).unsqueeze(-1)
+        mask = {int(zoom): mask[zoom].repeat_interleave(self.n_samples, dim=0) for zoom in mask.keys()}
 
         indices = {k: v.repeat_interleave(self.n_samples, dim=0) for k, v in sample_dict.items()}
         emb = {k: v.repeat_interleave(self.n_samples, dim=0) for k, v in emb.items()}
@@ -34,9 +35,9 @@ class LightningProbabilisticModel(pl.LightningModule):
             sample_dict_chunk = indices[start:end] if torch.is_tensor(indices) else {k: v[start:end] for k, v in indices.items()}
             
             output_chunk = self._predict_step(
-                source=source[start:end],
-                target=target[start:end],
-                mask=mask[start:end],
+                source={int(zoom): source[zoom][start:end] for zoom in source.keys()},
+                target={int(zoom): target[zoom][start:end] for zoom in target.keys()},
+                mask={int(zoom): mask[zoom][start:end] for zoom in mask.keys()},
                 emb=emb_chunk,
                 coords_input=coords_input[start:end],
                 coords_output=coords_output[start:end],
